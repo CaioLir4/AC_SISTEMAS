ad580746bd30e80209ea9932f0226cb6487fabd1
CaioLir4 <caiodevanalista@gmail.com>
2024-05-08 02:43:51 -0300

    up

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, send_file
-from peewee import Model, SqliteDatabase, CharField, DateTimeField, TextField
+from peewee import Model, SqliteDatabase, CharField, DateTimeField, TextField, IntegerField
 from datetime import datetime
-import os
 import xlsxwriter
 
 app = Flask(__name__)
@@ -11,6 +10,19 @@ db = SqliteDatabase('atendimentos.db')
 
 
 # Definindo o modelo de dados
+class Cliente(Model):
+    codigo = CharField()
+    nome = CharField()
+    responsavel = CharField()
+    telefone = CharField()
+    numero_maquinas = IntegerField()
+    valor_mensalidade = IntegerField()
+    acessos_maquinas = TextField()
+
+    class Meta:
+        database = db
+
+
 class Atendimento(Model):
     empresa = CharField()
     cliente = CharField()
@@ -24,11 +36,73 @@ class Atendimento(Model):
 
 # Criando as tabelas se não existirem
 db.connect()
-db.create_tables([Atendimento])
+db.create_tables([Cliente, Atendimento])
 
+
+# Rota para listar os clientes
+@app.route('/clientes')
+def listar_clientes():
+    clientes = Cliente.select()
+    return render_template('lista_clientes.html', clientes=clientes)
+
+
+# Rota para adicionar um novo cliente
+@app.route('/novo_cliente', methods=['GET', 'POST'])
+def novo_cliente():
+    if request.method == 'POST':
+        codigo = request.form['codigo']
+        nome = request.form['nome']
+        responsavel = request.form['responsavel']
+        telefone = request.form['telefone']
+        numero_maquinas = request.form['numero_maquinas']
+        valor_mensalidade = request.form['valor_mensalidade']
+        acessos_maquinas = request.form['acessos_maquinas']
+        Cliente.create(
+            codigo=codigo,
+            nome=nome,
+            responsavel=responsavel,
+            telefone=telefone,
+            numero_maquinas=numero_maquinas,
+            valor_mensalidade=valor_mensalidade,
+            acessos_maquinas=acessos_maquinas
+        )
+        return redirect(url_for('listar_clientes'))
+    return render_template('novo_cliente.html')
+
+
+# Rota para editar um cliente
+@app.route('/editar_cliente/<int:id>', methods=['GET', 'POST'])
+def editar_cliente(id):
+    cliente = Cliente.get_or_none(id=id)
+    if cliente:
+        if request.method == 'POST':
+            cliente.nome = request.form['nome']
+            cliente.email = request.form['email']
+            cliente.save()
+            return redirect(url_for('listar_clientes'))
+        return render_template('editar_cliente.html', cliente=cliente)
+    else:
+        return 'Cliente não encontrado', 404
+
+
+
+# Rota para excluir um cliente
+@app.route('/excluir_cliente/<int:id>')
+def excluir_cliente(id):
+    cliente = Cliente.get_or_none(id=id)
+    if cliente:
+        cliente.delete_instance()
+        return redirect(url_for('listar_clientes'))
+    else:
+        return 'Cliente não encontrado', 404
+
+
+@app.route('/')
+def pagina_inicial():
+    return render_template('menu.html')
 
 # Rota para listar os atendimentos
-@app.route('/')
+@app.route('/lista_atendimentos')
 def listar_atendimentos():
     atendimentos = Atendimento.select()
     return render_template('lista_atendimentos.html', atendimentos=atendimentos)
@@ -37,16 +111,22 @@ def listar_atendimentos():
 # Rota para adicionar um novo atendimento
 @app.route('/novo_atendimento', methods=['GET', 'POST'])
 def novo_atendimento():
+    clientes = Cliente.select()
     colaboradores = ['Colaborador 1', 'Colaborador 2',
-                     'Colaborador 3']  # Substitua com os nomes reais dos colaboradores
+                     'Colaborador 3']
     if request.method == 'POST':
         empresa = request.form['empresa']
-        cliente = request.form['cliente']
+        cliente = request.form.get('cliente')
         atendente = request.form['atendente']
         observacao = request.form['observacao']
-        Atendimento.create(empresa=empresa, cliente=cliente, atendente=atendente, observacao=observacao)
+        Atendimento.create(
+            empresa=empresa,
+            cliente=cliente,
+            atendente=atendente,
+            observacao=observacao
+        )
         return redirect(url_for('listar_atendimentos'))
-    return render_template('novo_atendimento.html', colaboradores=colaboradores)
+    return render_template('novo_atendimento.html', clientes=clientes, colaboradores=colaboradores)
 
 
 # Rota para visualizar a observação completa de um atendimento
@@ -58,90 +138,63 @@ def visualizar_observacao(id):
     else:
         return 'Atendimento não encontrado', 404
 
+# Rota para buscar atendimentos
+@app.route('/buscar_atendimentos')
+def buscar_atendimentos():
+    query = request.args.get('query')
+    atendimentos = Atendimento.select().where(Atendimento.empresa.contains(query) | Atendimento.cliente.contains(query) | Atendimento.atendente.contains(query))
+    return render_template('lista_atendimentos.html', atendimentos=atendimentos)
 
-# Rota para baixar o banco de dados em formato xlsx
-@app.route('/baixar_xlsx')
-def baixar_xlsx():
-    atendimentos = Atendimento.select()
-    output_path = 'atendimentos.xlsx'
+
+# Rota para buscar clientes
+@app.route('/buscar_clientes')
+def buscar_clientes():
+    query = request.args.get('query')
+    clientes = Cliente.select().where(Cliente.nome.contains(query))
+    return render_template('lista_clientes.html', clientes=clientes)
+
+@app.route('/baixar_clientes_xlsx')
+def baixar_clientes_xlsx():
+    # Dados dos clientes
+    clientes = Cliente.select()
+    clientes_data = [(cliente.nome, cliente.telefone) for cliente in clientes]
+
+    # Criar o arquivo XLSX para clientes
+    output_path = 'clientes.xlsx'
     workbook = xlsxwriter.Workbook(output_path)
-    worksheet = workbook.add_worksheet()
+    worksheet = workbook.add_worksheet('Clientes')
 
-    # Escrevendo cabeçalhos
-    headers = ['Empresa', 'Cliente', 'Atendente', 'Observação', 'Data e Hora']
-    for col, header in enumerate(headers):
-        worksheet.write(0, col, header)
-
-    # Escrevendo os dados
-    for row, atendimento in enumerate(atendimentos, start=1):
-        worksheet.write(row, 0, atendimento.empresa)
-        worksheet.write(row, 1, atendimento.cliente)
-        worksheet.write(row, 2, atendimento.atendente)
-        worksheet.write(row, 3, atendimento.observacao)
-        worksheet.write(row, 4, atendimento.data_hora.strftime('%d/%m/%Y %H:%M:%S'))
+    # Escrever dados dos clientes
+    for row, data in enumerate(clientes_data):
+        for col, value in enumerate(data):
+            worksheet.write(row, col, value)
 
     workbook.close()
+
+    # Enviar o arquivo de clientes para download
     return send_file(output_path, as_attachment=True)
 
-# Rota para baixar o banco de dados em formato XML
-@app.route('/baixar_xml')
-def baixar_xml():
+@app.route('/baixar_atendimentos_xlsx')
+def baixar_atendimentos_xlsx():
+    # Dados dos atendimentos
     atendimentos = Atendimento.select()
-    xml_data = '<?xml version="1.0" encoding="UTF-8" ?>\n<atendimentos>\n'
-    for atendimento in atendimentos:
-        xml_data += f'    <atendimento>\n        <empresa>{atendimento.empresa}</empresa>\n        <cliente>{atendimento.cliente}</cliente>\n        <atendente>{atendimento.atendente}</atendente>\n        <observacao>{atendimento.observacao}</observacao>\n        <data_hora>{atendimento.data_hora.strftime("%d/%m/%Y %H:%M:%S")}</data_hora>\n    </atendimento>\n'
-    xml_data += '</atendimentos>'
-    response = app.response_class(xml_data, mimetype='application/xml')
-    response.headers.set('Content-Disposition', 'attachment', filename='atendimentos.xml')
-    return response
+    atendimentos_data = [(atendimento.empresa, atendimento.cliente, atendimento.atendente, atendimento.observacao) for atendimento in atendimentos]
 
-# Rota para baixar o banco de dados em formato CSV
-@app.route('/baixar_csv')
-def baixar_csv():
-    atendimentos = Atendimento.select()
-    csv_data = 'Empresa,Cliente,Atendente,Observação,Data e Hora\n'
-    for atendimento in atendimentos:
-        csv_data += f'{atendimento.empresa},{atendimento.cliente},{atendimento.atendente},"{atendimento.observacao}",{atendimento.data_hora.strftime("%d/%m/%Y %H:%M:%S")}\n'
-    response = app.response_class(csv_data, mimetype='text/csv')
-    response.headers.set('Content-Disposition', 'attachment', filename='atendimentos.csv')
-    return response
+    # Criar o arquivo XLSX para atendimentos
+    output_path = 'atendimentos.xlsx'
+    workbook = xlsxwriter.Workbook(output_path)
+    worksheet = workbook.add_worksheet('Atendimentos')
 
-# Rota para baixar o banco de dados em formato TXT
-@app.route('/baixar_txt')
-def baixar_txt():
-    atendimentos = Atendimento.select()
-    txt_data = 'Empresa | Cliente | Atendente | Observação | Data e Hora\n'
-    for atendimento in atendimentos:
-        txt_data += f'{atendimento.empresa} | {atendimento.cliente} | {atendimento.atendente} | {atendimento.observacao} | {atendimento.data_hora.strftime("%d/%m/%Y %H:%M:%S")}\n'
-    response = app.response_class(txt_data, mimetype='text/plain')
-    response.headers.set('Content-Disposition', 'attachment', filename='atendimentos.txt')
-    return response
+    # Escrever dados dos atendimentos
+    for row, data in enumerate(atendimentos_data):
+        for col, value in enumerate(data):
+            worksheet.write(row, col, value)
 
-# Rota para editar um atendimento
-@app.route('/editar_atendimento/<int:id>', methods=['GET', 'POST'])
-def editar_atendimento(id):
-    atendimento = Atendimento.get_or_none(id=id)
-    if atendimento:
-        if request.method == 'POST':
-            atendimento.empresa = request.form['empresa']
-            atendimento.cliente = request.form['cliente']
-            atendimento.atendente = request.form['atendente']
-            atendimento.observacao = request.form['observacao']
-            atendimento.save()
-            return redirect(url_for('listar_atendimentos'))
-        return render_template('editar_atendimento.html', atendimento=atendimento)
-    else:
-        return 'Atendimento não encontrado', 404
+    workbook.close()
 
-# Rota para excluir um atendimento
-@app.route('/excluir_atendimento/<int:id>')
-def excluir_atendimento(id):
-    atendimento = Atendimento.get_or_none(id=id)
-    if atendimento:
-        atendimento.delete_instance()
-        return redirect(url_for('listar_atendimentos'))
-    else:
-        return 'Atendimento não encontrado', 404
+    # Enviar o arquivo de atendimentos para download
+    return send_file(output_path, as_attachment=True)
+
 
 
 
